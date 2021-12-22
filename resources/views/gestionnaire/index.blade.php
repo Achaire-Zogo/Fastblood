1af298f66fc4cbec17cf517caa9094973227a9f7
dylan-zogo <dylanabouma@gmail.com>
2021-12-22 17:47:10 +0100

    Update index.blade.php

--- resources/views/gestionnaire/index.blade.php
+++ resources/views/gestionnaire/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.backend')
 @section('content')
 <section class="section">
+        <a href="#" class="btn btn-primary">Nouveau don de sang</a><br>
         <div class="row ">
+
             <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
                 <div class="card">
                     <div class="card-statistic-4">
