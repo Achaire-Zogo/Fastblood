2d5d53adf57ff8b50f7eff3cb1b2fa79608a7bbe
vignol <teumoubande@gmail.com>
2021-12-23 04:48:46 +0100

    commit
    co

--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,7 @@ Route::get('/dashboard/Responsable', [App\Http\Controllers\responsable\Responsab
 
 //route pour le gestionnaire
 Route::get('/dashboard/gestionnaire', [App\Http\Controllers\gestionnaire\GestionnaireController::class, 'index'])->name('gestionnaire');
-
+Route::view('/groupe', 'gestionnaire/group' )->name('group');
 
 //route pour le user
 Route::get('/dashboard/user', [App\Http\Controllers\user\UserController::class, 'index'])->name('user');
