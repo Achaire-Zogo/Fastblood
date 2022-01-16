f01dc104d8671d825f6afdea7914f167ea3d33bb
dylan-zogo <dylanabouma@gmail.com>
2022-01-16 10:21:03 +0100

    Update DemanderController.php

--- app/Http/Controllers/user/DemanderController.php
+++ app/Http/Controllers/user/DemanderController.php
@@ -6,6 +6,7 @@ use App\Models\Sos;
 use App\Models\BloodBank;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Brian2694\Toastr\Facades\Toastr;
 
 class DemanderController extends Controller
 {
